04ce9f502a75f280760c8c3128612731edc70302
rainett <96131229+rainett@users.noreply.github.com>
2023-05-31 13:29:35 +0300

    added config server with native and vault demo properties

---
+++ config-microservice/src/main/java/io/github/configmicroservice/ConfigMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.configmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigMicroserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMicroserviceApplication.class, args);
+	}
+}
