34829bce1ed67c36cbede85d589d778b1517a9ca
rainett <96131229+rainett@users.noreply.github.com>
2023-06-01 13:43:44 +0300

    created admin microservice, updated discovery and configs

---
+++ admin-microservice/src/main/java/io/rainett/adminmicroservice/AdminMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.rainett.adminmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminMicroserviceApplication.class, args);
+    }
+
+}
---
+++ admin-microservice/src/test/java/io/rainett/adminmicroservice/AdminMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rainett.adminmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
